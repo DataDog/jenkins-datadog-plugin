96071124899225920086a29a66b29376212bc663
k_kitamura <k_kitamura@monotaro.com>
2016-08-01 13:34:58 +0900

    Not use getDuration() method

--- src/main/java/org/datadog/jenkins/plugins/datadog/DatadogBuildListener.java
+++ src/main/java/org/datadog/jenkins/plugins/datadog/DatadogBuildListener.java
@@ -208,7 +208,7 @@ public class DatadogBuildListener extends RunListener<Run>
 
     // Assemble JSON
     long starttime = run.getStartTimeInMillis() / DatadogBuildListener.THOUSAND_LONG; // ms to s
-    double duration = run.getDuration() / DatadogBuildListener.THOUSAND_DOUBLE; // ms to s
+    double duration = System.currentTimeMillis() / DatadogBuildListener.THOUSAND_DOUBLE - (double) starttime; // ms to s
     long endtime = starttime + (long) duration; // ms to s
     JSONObject builddata = new JSONObject();
     builddata.put("starttime", starttime); // long
