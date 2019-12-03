4cbd947d3c1ed3c992858ffe413c1f7a113a21a0
Gregory <gregory.zussa@datadoghq.com>
2019-12-03 17:44:24 +0100

    clean up

--- src/test/java/org/datadog/jenkins/plugins/datadog/DatadogUtilitiesTest.java
+++ src/test/java/org/datadog/jenkins/plugins/datadog/DatadogUtilitiesTest.java
@@ -48,21 +48,21 @@ public class DatadogUtilitiesTest {
 
     @Test
     public void testMerge(){
-        Map emptyTags = new HashMap<>();
+        Map<String, Set<String>> emptyTags = new HashMap<>();
         Assert.assertTrue(DatadogUtilities.merge(null, null).equals(new HashMap<String, Set<String>>()));
         Assert.assertTrue(DatadogUtilities.merge(emptyTags, null).equals(new HashMap<String, Set<String>>()));
         Assert.assertTrue(DatadogUtilities.merge(null, emptyTags).equals(new HashMap<String, Set<String>>()));
 
-        Map assertionTags = new HashMap<String, Set<String>>();
+        Map<String, Set<String>> assertionTags = new HashMap<>();
         assertionTags.put("name1", new HashSet<String>());
-        Map nullTagValue = new HashMap<>();
+        Map<String, Set<String>> nullTagValue = new HashMap<>();
         nullTagValue.put("name1", null);
         Assert.assertTrue(DatadogUtilities.merge(null, nullTagValue).equals(assertionTags));
         Assert.assertTrue(DatadogUtilities.merge(nullTagValue, nullTagValue).equals(assertionTags));
         Assert.assertTrue(DatadogUtilities.merge(nullTagValue, null).toString().equals(assertionTags.toString()));
         Assert.assertTrue(DatadogUtilities.merge(nullTagValue, emptyTags).toString().equals(assertionTags.toString()));
         Assert.assertTrue(DatadogUtilities.merge(emptyTags, nullTagValue).toString().equals(assertionTags.toString()));
-        Map emptyTagValue = new HashMap<>();
+        Map<String, Set<String>> emptyTagValue = new HashMap<>();
         emptyTagValue.put("name1", new HashSet<String>());
         Assert.assertTrue(DatadogUtilities.merge(emptyTagValue, null).toString().equals(assertionTags.toString()));
         Assert.assertTrue(DatadogUtilities.merge(null, emptyTagValue).toString().equals(assertionTags.toString()));
@@ -70,12 +70,12 @@ public class DatadogUtilitiesTest {
         Assert.assertTrue(DatadogUtilities.merge(emptyTags, emptyTagValue).toString().equals(assertionTags.toString()));
         Assert.assertTrue(DatadogUtilities.merge(nullTagValue, emptyTagValue).toString().equals(assertionTags.toString()));
         Assert.assertTrue(DatadogUtilities.merge(emptyTagValue, nullTagValue).toString().equals(assertionTags.toString()));
-        Map n1v1Tag = new HashMap<>();
-        Set v1 = new HashSet<>();
+        Map<String, Set<String>> n1v1Tag = new HashMap<>();
+        Set<String> v1 = new HashSet<>();
         v1.add("value1");
         n1v1Tag.put("name1", v1);
-        assertionTags = new HashMap<String, Set<String>>();
-        Set assertionV1 = new HashSet<>();
+        assertionTags = new HashMap<>();
+        Set<String> assertionV1 = new HashSet<>();
         assertionV1.add("value1");
         assertionTags.put("name1", assertionV1);
         Assert.assertTrue(DatadogUtilities.merge(n1v1Tag, null).toString().equals(assertionTags.toString()));
@@ -87,28 +87,28 @@ public class DatadogUtilitiesTest {
         Assert.assertTrue(DatadogUtilities.merge(n1v1Tag, emptyTagValue).toString().equals(assertionTags.toString()));
         Assert.assertTrue(DatadogUtilities.merge(emptyTagValue, n1v1Tag).toString().equals(assertionTags.toString()));
 
-        Map n1v1TagCopy = new HashMap<>();
-        Set v1Copy = new HashSet<>();
+        Map<String, Set<String>> n1v1TagCopy = new HashMap<>();
+        Set<String> v1Copy = new HashSet<>();
         v1Copy.add("value1");
         n1v1TagCopy.put("name1", v1Copy);
         Assert.assertTrue(DatadogUtilities.merge(n1v1TagCopy, n1v1Tag).toString().equals(assertionTags.toString()));
 
-        Map n1v2Tag = new HashMap<>();
-        Set v2 = new HashSet<>();
+        Map<String, Set<String>> n1v2Tag = new HashMap<>();
+        Set<String> v2 = new HashSet<>();
         v2.add("value2");
         n1v2Tag.put("name1", v2);
-        assertionTags = new HashMap<String, Set<String>>();
-        Set assertionValues = new HashSet<>();
+        assertionTags = new HashMap<>();
+        Set<String> assertionValues = new HashSet<>();
         assertionValues.add("value1");
         assertionValues.add("value2");
         assertionTags.put("name1", assertionValues);
         Assert.assertTrue(DatadogUtilities.merge(n1v2Tag, n1v1Tag).toString().equals(assertionTags.toString()));
 
-        Map n2v1Tag = new HashMap<>();
+        Map<String, Set<String>> n2v1Tag = new HashMap<>();
         v1 = new HashSet<>();
         v1.add("value1");
         n2v1Tag.put("name2", v1);
-        assertionTags = new HashMap<String, Set<String>>();
+        assertionTags = new HashMap<>();
         assertionV1 = new HashSet<>();
         assertionV1.add("value1");
         assertionTags.put("name1", assertionV1);
@@ -120,8 +120,8 @@ public class DatadogUtilitiesTest {
         v1 = new HashSet<>();
         v1.add("value1");
         n2v1Tag.put("name2", v1);
-        Map n2v1v2Tag = new HashMap<>();
-        Set v1v2 = new HashSet<>();
+        Map<String, Set<String>> n2v1v2Tag = new HashMap<>();
+        Set<String> v1v2 = new HashSet<>();
         v1v2.add("value1");
         v1v2.add("value2");
         n2v1v2Tag.put("name2", v1v2);
@@ -132,26 +132,6 @@ public class DatadogUtilitiesTest {
         assertionTags.put("name2", assertionValues);
         Assert.assertTrue(DatadogUtilities.merge(n2v1Tag, n2v1v2Tag).toString() + " - "+ assertionTags.toString(),
                 DatadogUtilities.merge(n2v1Tag, n2v1v2Tag).toString().equals(assertionTags.toString()));
-
-    }
-
-    @Test
-    public void testComputeTagListFromVarList(){
-
-    }
-
-    @Test
-    public void testGetTagsFromGlobalJobTags(){
-
-    }
-
-    @Test
-    public void testIsBlacklisted(){
-
-    }
-
-    @Test
-    public void testIsWhitelisted(){
 
     }
 
